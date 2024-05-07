993597907a9df0ca82d3956acd8d7ca0bd2fbea7
TechDev <89547375+TechDevOfficial@users.noreply.github.com>
2024-05-07 19:13:23 +0200

    Update StoryMenuState.hx

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -35,7 +35,7 @@ class StoryMenuState extends MusicBeatState
 	];
 	var curDifficulty:Int = 1;
 
-	public static var weekUnlocked:Array<Bool> = [true, true, true, true, true, true, true];
+	public static var weekUnlocked:Array<Bool> = [true, true, true, true, true, true, true, false];
 
 	var weekCharacters:Array<Dynamic> = [
 		['gf', 'bf', ''],
